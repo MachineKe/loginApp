0834e6bbe5246478d85c0531a20741f5a2d47c52
machineke <markarapsoi72@gmail.com>
2024-03-21 21:35:24 +0300

    added appflow build script

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.loginapp.app',
-  appName: 'lohinapp',
+  appName: 'loginapp',
   webDir: 'build',
   server: {
     androidScheme: 'https'
